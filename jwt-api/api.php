c630ab0c7b5dd1eb7cba3015a3dba8022a768f62
lalit <tiwarilalit601@gmail.com>
2023-06-12 12:20:31 +0530

    Update message

--- jwt-api/api.php
+++ jwt-api/api.php
@@ -1762,7 +1762,7 @@ class Api extends Rest
                         $stmt->bindParam(":user_id", $user_id);
                         $stmt->bindParam(":product_id", $product_id);
                         if ($stmt->execute()) {
-                            $response = ["status" => true, "code" => 200, "Message" => "Product has been added in your favourite list."];
+                            $response = ["status" => true, "code" => 200, "Message" => "Product has been removed from your favourite list."];
                             $this->returnResponse($response);
                         } else {
                             $response = ["status" => false, "code" => 400, "Message" => "Something is wrong."];
@@ -1775,7 +1775,7 @@ class Api extends Rest
                         $stmt->bindParam(':user_id', $user_id);
                         $stmt->bindParam(':product_id', $product_id);
                         if ($stmt->execute()) {
-                            $response = ["status" => true, "code" => 200, "Message" => "Product has been removed from your favourite list."];
+                            $response = ["status" => true, "code" => 200, "Message" => "Product has been added in your favourite list."];
                             $this->returnResponse($response);
                         } else {
                             $response = ["status" => false, "code" => 400, "Message" => "Something is wrong."];
