de9d811307295b7293b36286af8a21f5d63c1aaf
lalit <tiwarilalit601@gmail.com>
2023-07-11 13:29:39 +0530

    Worked on chat for app

--- chat/chat.php
+++ chat/chat.php
@@ -170,7 +170,11 @@ if (Login::isLogged()) {
 		};
 	</script>
 
-<?php } else
-	header('Location: index.php'); ?>
+<?php }  ?>
 
-<?php require('templates/footer.php'); ?>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+	
+	</body>
+</html>
